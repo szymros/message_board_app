37458a5f517b9100ae0b75faac1b1a9763a480bc
szymek <szymon.roslaniecc@gmail.com>
2021-03-29 09:13:19 +0200

    customized hashed(changed salt size and rounds)

--- resources/user.py
+++ resources/user.py
@@ -2,7 +2,7 @@ from flask_restful import Resource
 from flask import request
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt
-from passlib.hash import pbkdf2_sha256     
+from passlib.hash import pbkdf2_sha256
 
 from db import db
 from blocklist import BLOCKLIST
@@ -11,6 +11,10 @@ from models.user import UserModel
 from schemas.user import UserSchema
 
 user_schema = UserSchema()
+custom_hash = pbkdf2_sha256.using(rounds=30000,salt_size=16)
+                                #(rounds, salt, salt_size) 
+                                # rounds = how many times is hashed
+                                # salt = added characters and salt_size = how many of these characters
 
 class UserResource(Resource):
     @classmethod
@@ -43,6 +47,7 @@ class CreateUser(Resource):
             new_user.save_to_db()
             return user_schema.dump(new_user), 200
         return {'msg' : 'couldnt create new user'}
+        
         
 class UserLogin(Resource):
     @classmethod
